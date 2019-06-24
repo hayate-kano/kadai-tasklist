768dfa56a5c15df3cef8e21bd9ec87eeb7795a74
hayate.kano <kano-hayate99@outlook.jp>
2019-06-24 14:38:55 +0000

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   root to: 'toppages#index'
   root to: 'tasks#index'
   
+  get 'signup', to: 'users#new'
+  resources :users, only: [:new, :create]
   resources :tasks
 end
